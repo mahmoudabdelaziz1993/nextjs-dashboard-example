56da1ad6e1a558eeffc5a9492a949ff616f0488f
mahmoudabdelaziz1993 <mahmoudabdelaziz1993@outlook.com>
2025-01-23 03:14:15 +0200

    mutate and validate forms

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,16 +1,11 @@
-import CardWrapper, { Card } from '@/app/ui/dashboard/cards';
+import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import {
-    fetchCardData, fetchLatestInvoices,
-    // fetchRevenue
-} from '../../lib/data';
 import { Suspense } from 'react';
-import { CardSkeleton, CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
 export default async function Dashboard() {
-    // const revenue = await fetchRevenue();
-    // const latestInvoices = await fetchLatestInvoices();
+
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-3xl`}>
